8387e1ebdd52a6e3434b9c424ec94e6d7cdbe4d6
abdulmanan-js-dev <fokamystic@gmail.com>
2025-12-18 19:33:38 -0500

    Add dynamic card creation when submit for a task and update the ui.js with more functions.

--- 15. todo-app/src/app.js
+++ 15. todo-app/src/app.js
@@ -1,11 +1,19 @@
-import { getQuote } from "./ui.js";
+import { getQuote, clearInput, createCard, updateCounter } from "./ui.js";
 
 const searchInput = document.querySelector("#search-input");
-const toggleBtn = document.querySelector("#themeToggle");
 const sidebarWrapper = document.querySelector("#sidebar-wrapper");
 const motivationContent = document.querySelector("#motivation-content");
 const totalTasks = document.querySelector("#total-tasks");
 const completedTasks = document.querySelector("#completed-tasks");
+const form = document.querySelector("#card-form");
+const title = document.querySelector("#title");
+const description = document.querySelector("#description");
+const priority = document.querySelector("#priority-emoji");
+const taskList = document.querySelector("#task-list");
+
+const taskArray = JSON.parse(localStorage.getItem("tasks")) || [];
+createCard(taskArray, taskList);
+updateCounter(totalTasks, completedTasks, taskArray);
 
 const apiUrl = "https://api.freeapi.app/api/v1/public/quotes/quote/random";
 
@@ -13,8 +21,50 @@ document.addEventListener("DOMContentLoaded", () => {
     getQuote(apiUrl, motivationContent);
 });
 
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
 
+    const taskTitle = title.value.trim();
+    if (!taskTitle) return;
+    clearInput(title);
 
+    const taskDescription = description.value.trim();
+    if (!taskDescription) return;
+    clearInput(description);
 
+    const taskPriority = priority.options[priority.selectedIndex].text;
+    if (!taskPriority || taskPriority === "Choose priority") return;
+    clearInput(priority);
 
+    const task = {
+        id: Date.now(),
+        taskTitle,
+        taskDescription,
+        taskPriority,
+        isCompleted: false,
+    };
 
+    console.log(task);
+    taskArray.push(task);
+    updateCounter(totalTasks, completedTasks, taskArray);
+    createCard(taskArray, taskList);
+    // localStorage.setItem("tasks", JSON.stringify(taskArray));
+});
+
+taskList.addEventListener("click", (e) => {
+    const button = e.target.closest("button");
+    if (!button) return;
+
+    const id = parseInt(button.dataset.id);
+    const action = button.dataset.action;
+
+    if (action === "complete") {
+        taskArray.forEach((task) => {
+            task.id == id
+                ? (task.isCompleted = true)
+                : (task.isCompleted = false);
+
+            console.log("updated", task);
+        });
+    }
+});
--- 15. todo-app/src/ui.js
+++ 15. todo-app/src/ui.js
@@ -7,7 +7,6 @@ export async function getQuote(url, element) {
         }
 
         const data = await response.json();
-        console.log(data);
         element.textContent = `"${data.data.content}" — ${data.data.author}`;
     } catch (error) {
         element.textContent = "Stay positive. Keep moving forward 💪";
@@ -15,4 +14,75 @@ export async function getQuote(url, element) {
     }
 }
 
+export function clearInput(element) {
+    element.value = "";
+}
 
+export function createCard(array, element) {
+    element.innerHTML = "";
+
+    array.forEach((obj) => {
+        element.innerHTML += `
+                            <div
+                                class="bg-sidebarBg border border-borderColor px-3 py-4 rounded-xl max-w-xs md:max-w-sm flex flex-col gap-2 shadow-sm"
+                            >
+                                <div>
+                                    <p
+                                        class="font-bold text-sm text-primaryText"
+                                    >
+                                        Title:
+                                    </p>
+                                    <p class="px-2 text-sm text-secondaryText">
+                                        ${obj.taskTitle}
+                                    </p>
+                                </div>
+                                <div>
+                                    <p
+                                        class="font-bold text-sm text-primaryText"
+                                    >
+                                        Description:
+                                    </p>
+                                    <p class="px-2 text-sm text-secondaryText break-all">
+                                        ${obj.taskDescription}
+                                    </p>
+                                </div>
+                                <div>
+                                    <p
+                                        class="font-bold text-sm text-primaryText"
+                                    >
+                                        Priority:
+                                    </p>
+                                    <p class="px-2 text-sm text-secondaryText">
+                                        ${obj.taskPriority}
+                                    </p>
+                                </div>
+                                <div
+                                    class="flex justify-center gap-2 mt-2 text-center"
+                                >
+                                    <button
+                                        type="button"
+                                        data-id=${obj.id}
+                                        data-action="complete"
+                                        class="btn focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-activeItem"
+                                    >
+                                        Completed
+                                    </button>
+                                    <button
+                                        type="button"
+                                        data-id=${obj.id}
+                                        data-action="delete"
+                                        class="btn text-red-600 border-red-200 hover:bg-red-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-200"
+                                    >
+                                        Delete
+                                    </button>
+                                </div>
+                            </div>
+
+    `;
+    });
+}
+
+export function updateCounter(totalTasks, completedTasks, array) {
+    totalTasks.innerText = array.filter((task) => !task.isCompleted).length;
+    completedTasks.innerText = array.filter((task) => task.isCompleted).length;
+}
